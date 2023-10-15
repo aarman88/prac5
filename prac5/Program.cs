b588875d81c707c7dcd2d4c1b54e4987c144780f
Арман <@LAPTOP-8BLD2IN5>
2023-10-15 23:15:51 +0600

    Добавьте файлы проекта.

---
+++ prac5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            CallMethodWithException();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+    }
+
+    static void CallMethodWithException()
+    {
+        Console.WriteLine("Метод CallMethodWithException начал работу.");
+        GenerateException();
+        Console.WriteLine("Метод CallMethodWithException завершил работу.");
+    }
+
+    static void GenerateException()
+    {
+        Console.WriteLine("Метод GenerateException начал работу.");
+        // Генерируем исключение
+        throw new Exception("Это тестовое исключение.");
+        Console.WriteLine("Метод GenerateException завершил работу.");
+    }
+}
---
+++ prac5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("prac5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("prac5")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2e2e3c6c-072e-4299-8fa0-b59b1f03fa6a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
